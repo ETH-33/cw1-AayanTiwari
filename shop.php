85d1ea7f7cab7b80990f43e1c1d32ad3f7c6039d
AayanTiwari <130342101+AayanTiwari@users.noreply.github.com>
2023-07-16 20:48:43 +0545

    Connected database and retrieved the content

--- shop.php
+++ shop.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -41,6 +42,13 @@
     font-weight: bold;
   }
   </style>
+<?php
+		$Username = null;
+		if(!empty($_SESSION["Username"]))
+		{
+			$Username = $_SESSION["Username"];
+		}
+	?>
 </head>
 <body>
   <nav>
@@ -49,7 +57,7 @@
       <li><a href="shop.php">Shop</a></li>
       <li><a href="#"><span id="darkModeButton" onclick="toggleDarkMode()">Change Theme</span></a></li>
       <?php if($Username == null){echo '<li><a href="register.php?actiontype=register">Register</a></li>';} ?>
-			<?php if($Username == null){echo '<li><a href="login.php?r=5921b8e471bdd8a0b4348dfecd31620b">Login</a></li>';} else {echo '<li><a href="Logout.php">Logout</a></li>';} ?>
+      <?php if($Username == null){echo '<li><a href="login.php?r=5921b8e471bdd8a0b4348dfecd31620b">Login</a></li>';} else {echo '<li><a href="Logout.php">Logout</a></li>';} ?>
     </ul>
     <script>
     var isDarkModeEnabled = localStorage.getItem("darkModeEnabled");
@@ -75,8 +83,27 @@
     });
   </script>
   <div class="shop-container">
-	
-	</div>
+	<?php 
+		$conn = mysqli_connect("localhost","root","","shoe");
+		$sql = "SELECT * FROM `tbl_products` order by ProductPrice";
+		$Resulta = mysqli_query($conn,$sql);
+	?>	
+	<?php while($Rows = mysqli_fetch_array($Resulta)){
+	echo '	
+        <div class="shop">
+	<h4 style="text-align: center;">'.$Rows[2].'</h4>
+        <img style="border: 2px solid gray; border-radius: 10px;" src="data:image;base64,'.$Rows[8].'" alt="">
+        <div class="caption">
+	<p><span class="s-info">Product Name</span>: '.$Rows[1].'</p>
+	<p><span class="s-info">Size Available</span>: '.$Rows[3].'</p>
+	<p><span class="s-info">Colors Available</span>: '.$Rows[4].'</p>
+	<p><span class="s-info">Price</span>: Rs '.$Rows[5].'.00</p>
+        </div>
+	<center><a onclick="addToCartOnclick('.$Rows[0].');" href="#"  style="margin-bottom: 5px;" class="shbtn">Add to Cart</a></center>
+        </div>
+	';
+	}?>
+</div>
   <footer>Designed and built by Aayan Tiwari</footer>
 </body>
 
